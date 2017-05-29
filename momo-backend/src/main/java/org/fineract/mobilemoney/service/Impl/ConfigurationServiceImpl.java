c458b53912766ed7ef54ee30bdb500c1eeb3bbb8
Daniel Carlson <dancarl758@gmail.com>
2017-05-29 17:11:42 +0100

    Making develop uniform with master

---
+++ momo-backend/src/main/java/org/fineract/mobilemoney/service/Impl/ConfigurationServiceImpl.java
@@ -0,0 +1,33 @@
+package org.fineract.mobilemoney.service.Impl;
+
+import org.fineract.mobilemoney.domain.Configuration;
+import org.fineract.mobilemoney.service.ConfigurationService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+/**
+ * Created by daniel on 2/22/17.
+ */
+@Service
+public class ConfigurationServiceImpl implements ConfigurationService {
+    @Override
+    public Collection<Configuration> findAll() {
+        return null;
+    }
+
+    @Override
+    public Configuration findOne(Long id) {
+        return null;
+    }
+
+    @Override
+    public Configuration create(Configuration configuration) {
+        return null;
+    }
+
+    @Override
+    public Configuration update(Configuration configuration) {
+        return null;
+    }
+}
